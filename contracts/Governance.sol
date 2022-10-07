86e95059edd607bb6d6c2b6a25f9abb18b79bdd2
Cyril <cyril@chiliz.com>
2022-10-07 17:09:49 +0200

    fixes Activate Proposers Registry function

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -87,7 +87,7 @@ contract Governance is InjectorContextHolder, GovernorCountingSimple, GovernorSe
         require(!_registryActivated, "Governance: registry already activated");
         address[] memory currentValidatorSet = _stakingContract.getValidators();
         for (uint256 i = 0; i < currentValidatorSet.length; i++) {
-            _addProposer(currentValidatorSet[i]);
+            _addProposer(_stakingContract.getValidatorByOwner(currentValidatorSet[i]));
         }
         _registryActivated = true;
     }
