1604d654ad7650422e11cb66915f4f5f7a511b96
Giorgi Abuladze <giorgi.abuladze@chiliz.com>
2025-02-03 12:57:03 +0400

    Added EpochConsensusParams struct to keep traack of activeValidatorLength per epoch. Updated ctor. Added getActiveValidatorsLength(uint64 epoch)

--- contracts/ChainConfig.sol
+++ contracts/ChainConfig.sol
@@ -5,7 +5,7 @@ import "./Injector.sol";
 
 contract ChainConfig is InjectorContextHolder, IChainConfig {
 
-    event ActiveValidatorsLengthChanged(uint32 prevValue, uint32 newValue);
+    event ActiveValidatorsLengthChanged(uint32 prevValue, uint32 newValue, uint64 epoch);
     event EpochBlockIntervalChanged(uint32 prevValue, uint32 newValue);
     event MisdemeanorThresholdChanged(uint32 prevValue, uint32 newValue);
     event FelonyThresholdChanged(uint32 prevValue, uint32 newValue);
@@ -15,7 +15,7 @@ contract ChainConfig is InjectorContextHolder, IChainConfig {
     event MinStakingAmountChanged(uint256 prevValue, uint256 newValue);
 
     struct ConsensusParams {
-        uint32 activeValidatorsLength;
+        uint32 activeValidatorsLength; // depricated. use epochConsensusParams.activeValidatorLength
         uint32 epochBlockInterval;
         uint32 misdemeanorThreshold;
         uint32 felonyThreshold;
@@ -25,7 +25,19 @@ contract ChainConfig is InjectorContextHolder, IChainConfig {
         uint256 minStakingAmount;
     }
 
+    struct ActiveValidatorLength {
+        // (epoch => activeValidatorsLength)
+        mapping(uint64 => uint32) value;
+        // list of available epochs, sorted in asc order.
+        uint64[] epochs;
+    }
+
+    struct EpochConsensusParams {
+        ActiveValidatorLength activeValidatorLength;
+    }
+
     ConsensusParams private _consensusParams;
+    EpochConsensusParams private _epochConsensusParams;
 
     constructor(bytes memory constructorParams) InjectorContextHolder(constructorParams) {
     }
@@ -40,8 +52,9 @@ contract ChainConfig is InjectorContextHolder, IChainConfig {
         uint256 minValidatorStakeAmount,
         uint256 minStakingAmount
     ) external onlyInitializing {
-        _consensusParams.activeValidatorsLength = activeValidatorsLength;
-        emit ActiveValidatorsLengthChanged(0, activeValidatorsLength);
+        _epochConsensusParams.activeValidatorLength.value[0] = activeValidatorsLength;
+        _epochConsensusParams.activeValidatorLength.epochs.push(0);
+        emit ActiveValidatorsLengthChanged(0, activeValidatorsLength, 0);
         _consensusParams.epochBlockInterval = epochBlockInterval;
         emit EpochBlockIntervalChanged(0, epochBlockInterval);
         _consensusParams.misdemeanorThreshold = misdemeanorThreshold;
@@ -62,14 +75,64 @@ contract ChainConfig is InjectorContextHolder, IChainConfig {
         return _consensusParams;
     }
 
+    /// @notice depricated. use `getActiveValidatorsLength(uint64 epoch)` instead.
     function getActiveValidatorsLength() external view override returns (uint32) {
         return _consensusParams.activeValidatorsLength;
     }
 
+    function getActiveValidatorsLength(uint64 epoch) external view returns (uint32) {
+        ActiveValidatorLength storage avl = _epochConsensusParams.activeValidatorLength;
+
+        if (avl.value[epoch] > 0) {
+            return avl.value[epoch];
+        } else {
+            uint256 epochsLen = avl.epochs.length;
+            uint64 lastAvailableEpoch = avl.epochs[epochsLen - 1];
+            // If we don't have the value for the epoch, return the lastAvailable epoch value if possible.
+            // (actually, epoch == lastAvailableEpoch case should be covered by the if statement above, but whatever..)
+            if (epoch >= lastAvailableEpoch) {
+                return avl.value[lastAvailableEpoch];
+            } else {
+                // If we don't have the value for the epoch and epoch < lastAvailable
+                // binary search to find the closest epoch
+                uint256 left = 0;
+                uint256 right = epochsLen;
+                while (left < right) {
+                     uint256 mid = left + (right - left) / 2;
+                     if (avl.epochs[mid] <= epoch) {
+                         left = mid + 1;
+                     } else {
+                         right = mid;
+                     }
+                }
+                return avl.value[avl.epochs[left-1]];
+            }
+        }
+    }
+
+    function initActiveValidatorLengthEpochParam(uint64[] memory epochs, uint32[] memory lengths) public {
+        require(epochs.length == lengths.length, "IA"); // invalid arguments
+
+        ActiveValidatorLength storage avl = _epochConsensusParams.activeValidatorLength;
+        require(avl.epochs.length == 0, "AI"); // already initialized
+
+        uint256 len = epochs.length;
+        for (uint256 i = 0; i < len; i++) {
+            uint64 epoch = epochs[i];
+            avl.value[epoch] = lengths[i];
+            avl.epochs.push(epoch);
+        }
+    }
+
     function setActiveValidatorsLength(uint32 newValue) external override onlyFromGovernance {
-        uint32 prevValue = _consensusParams.activeValidatorsLength;
-        _consensusParams.activeValidatorsLength = newValue;
-        emit ActiveValidatorsLengthChanged(prevValue, newValue);
+        ActiveValidatorLength storage avl = _epochConsensusParams.activeValidatorLength;
+
+        // set new value for next epoch
+        uint64 nextEpoch = _stakingContract.nextEpoch();
+        uint32 prevValue = avl.value[avl.epochs[avl.epochs.length - 1]];
+        avl.value[nextEpoch] = newValue;
+        avl.epochs.push(nextEpoch);
+        emit ActiveValidatorsLengthChanged(prevValue, newValue, nextEpoch);
     }
 
     function getEpochBlockInterval() external view override returns (uint32) {
