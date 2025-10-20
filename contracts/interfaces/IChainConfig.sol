bd5e4506a3ee0c12ef1349b2d31d70e475cdb88f
Giorgi Abuladze <giorgi.abuladze@chiliz.com>
2025-10-20 18:28:14 +0400

    fix(BLK-3491): Sherlock #80 (#58)
    * Deleted setEpochBlockInterval() function
    
    * Deleted setEpochBlockInterval() from IChainConfig

--- contracts/interfaces/IChainConfig.sol
+++ contracts/interfaces/IChainConfig.sol
@@ -9,8 +9,6 @@ interface IChainConfig {
 
     function getEpochBlockInterval() external view returns (uint32);
 
-    function setEpochBlockInterval(uint32 newValue) external;
-
     function getMisdemeanorThreshold() external view returns (uint32);
 
     function setMisdemeanorThreshold(uint32 newValue) external;
