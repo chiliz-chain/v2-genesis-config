7ad27e75dae03f6f4dcb7c3b973f72acaf57b550
Giorgi Abuladze <giorgi.abuladze@chiliz.com>
2025-12-09 15:31:34 +0400

    Governance scripts for Sherlock high risk batch 1 (#94)

---
+++ scripts/SherlockHighRisk1.s.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Governance} from "../contracts/Governance.sol";
+
+/// @notice Scripts to deploy first batch of high risk tickets from the Sherlock audit
+///         Proposal 1: Staking + StakingPool + SystemReward: #92 + #121 (will include #120) + #71 + #73, #125 + #56, #134, #3
+
+
+contract Propose is Script {
+    function run() public {
+        address[] memory targets = new address[](3);
+        uint256[] memory values = new uint256[](3);
+        bytes[] memory calldatas = new bytes[](3);
+
+        targets[0] = 0x0000000000000000000000000000000000007004; // RuntimeUpgrade
+        targets[1] = 0x0000000000000000000000000000000000007004;
+        targets[2] = 0x0000000000000000000000000000000000007004;
+        calldatas[0] = abi.encodeWithSignature(
+            "upgradeSystemSmartContract(address,bytes,bytes)",
+            0x0000000000000000000000000000000000001000,
+            vm.getDeployedCode("Staking.sol:Staking"),
+            ""
+        );
+        calldatas[1] = abi.encodeWithSignature(
+            "upgradeSystemSmartContract(address,bytes,bytes)",
+            0x0000000000000000000000000000000000007001,
+            vm.getDeployedCode("StakingPool.sol:StakingPool"),
+            ""
+        );
+        calldatas[2] = abi.encodeWithSignature(
+            "upgradeSystemSmartContract(address,bytes,bytes)",
+            0x0000000000000000000000000000000000001002,
+            vm.getDeployedCode("SystemReward.sol:SystemReward"),
+            ""
+        );
+
+        vm.startBroadcast();
+        uint256 proposalId = Governance(payable(0x0000000000000000000000000000000000007002)).proposeWithCustomVotingPeriod(
+            targets,
+            values,
+            calldatas,
+            vm.envString("PROPOSAL_DESCRIPTION"),
+            vm.envUint("VOTING_PERIOD")
+        );
+        vm.stopBroadcast();
+        console.log("Proposal ID:", proposalId);
+    }
+}
+
+contract CastVote is Script {
+    function run() public {
+        vm.startBroadcast();
+        Governance(payable(0x0000000000000000000000000000000000007002)).castVote(vm.envUint("PROPOSAL_ID"), 1);
+        vm.stopBroadcast();
+    }
+}
+
+contract ProposalState is Script {
+    mapping(uint8 => string) internal stateNames;
+    function run() public {
+        stateNames[0] = "Pending";
+        stateNames[1] = "Active";
+        stateNames[2] = "Canceled";
+        stateNames[3] = "Defeated";
+        stateNames[4] = "Succeeded";
+        stateNames[5] = "Queued";
+        stateNames[6] = "Expired";
+        stateNames[7] = "Executed";
+        uint8 state = uint8(Governance(payable(0x0000000000000000000000000000000000007002)).state(vm.envUint("PROPOSAL_ID")));
+        console.log("Proposal State:", stateNames[state]);
+    }
+}
+
+contract Execute is Script {
+    function run() public {
+        address[] memory targets = new address[](3);
+        uint256[] memory values = new uint256[](3);
+        bytes[] memory calldatas = new bytes[](3);
+        string memory description = vm.envString("PROPOSAL_DESCRIPTION");
+        bytes32 descriptionHash = keccak256(abi.encodePacked(description));
+
+        targets[0] = 0x0000000000000000000000000000000000007004; // RuntimeUpgrade
+        targets[1] = 0x0000000000000000000000000000000000007004;
+        targets[2] = 0x0000000000000000000000000000000000007004;
+        calldatas[0] = abi.encodeWithSignature(
+            "upgradeSystemSmartContract(address,bytes,bytes)",
+            0x0000000000000000000000000000000000001000,
+            vm.getDeployedCode("Staking.sol:Staking"),
+            ""
+        );
+        calldatas[1] = abi.encodeWithSignature(
+            "upgradeSystemSmartContract(address,bytes,bytes)",
+            0x0000000000000000000000000000000000007001,
+            vm.getDeployedCode("StakingPool.sol:StakingPool"),
+            ""
+        );
+        calldatas[2] = abi.encodeWithSignature(
+            "upgradeSystemSmartContract(address,bytes,bytes)",
+            0x0000000000000000000000000000000000001002,
+            vm.getDeployedCode("SystemReward.sol:SystemReward"),
+            ""
+        );
+
+        vm.startBroadcast();
+        Governance(payable(0x0000000000000000000000000000000000007002)).execute(targets, values, calldatas, descriptionHash);
+        vm.stopBroadcast();
+    }
+}
