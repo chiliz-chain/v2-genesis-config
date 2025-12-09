8b865c9027e4a1d17c5ad737c6ba8b1c29c43c1e
Giorgi Abuladze <giorgi.abuladze@chiliz.com>
2025-12-09 18:55:58 +0400

    fix(BLK-3498): Sherlock #66 (#70)
    * Moved getValidators logic to an internal function and added getValidatorsAtEpoch() function.
    
    * Added epoch argument to _totalDelegatedToValidator()
    
    * Added test for quorum()
    
    * Fixed typo
    
    * Use staking.getValidatorsAtEpoch() in governance._votingSupply(). Refactored governance._validatorVotingPowerAt().
    
    * Added EpochConsensusParams struct to keep traack of activeValidatorLength per epoch. Updated ctor. Added getActiveValidatorsLength(uint64 epoch)
    
    * Added getValidatorsAtEpoch() to return list of active validators per epoch
    
    * Added tests
    
    * Renamed ActiveValidatorLength to Uint32Param
    
    * Removed deprication notice from getActiveValidatorsLength(), call the new function instead of returning value from _consensusParams.
    
    * Renamed Uint32Param -> EpochToValue
    
    * Added _activeValidatorsListPerEpoch and updated addValidator to use it
    
    * Updated _removeValidatorFromActiveList to use _activeValidatorsListPerEpoch
    
    * Updated _activateValidator to use _activeValidatorsListPerEpoch
    
    * Added test for slash()
    
    * Updated releaseValidatorFromJail to use _activeValidatorsListPerEpoch
    
    * use _activeValidatorsListPerEpoch in _getValidators
    
    * Added tests for addValidator, removeValidator, activateValidator, slash, releaseValidatorFromJail
    
    * Added another test for quorum that verifies the quorum calculation when active validators list changes per epoch
    
    * Removed status check from _validatorVotingPowerAtEpoch
    
    * Added scripts to measure gas usage
    
    * Added script dir to config
    
    * Added onlyFromGovernance to initActiveValidatorLengthEpochParam()
    
    * Added test for sherlock #13 (#72)
    
    * Added initActiveValidatorsListPerEpoch() (#82)
    
    * fix(BLK-3499): Sherlock #23 (#71)
    
    * Added misdemeanorThreshold to EpochConsensusParams. Updated getter & setter. Added test.
    
    * Moved the search logic to separate function.
    
    * Moved Uint32Param setter logic to separate function.
    
    * Added getMisdemeanorThreshold() to IChainConfig
    
    * Updated _calcValidatorSnapshotEpochPayout() and all functions that call it.
    
    * Added test for test_getValidatorFeeAtEpoch
    
    * Updated code to reflect the renaming of Uint32Param -> EpochToValue
    
    * Added test for RuntimeUpgrade.upgradeSystemSmartContract
    
    * Simulate contract upgrade in FakeRuntimeUpgradeEvmHook
    
    * pass vm to FakeRunTimeUpgradeEvmHook
    
    * Added onlyFromGovernance to initMisdemeanorThresholdParam
    
    * fix(BLK-3507): Sherlock #77 (#75)
    
    * Updated sort logic in _getValidators()
    
    * Added init function for ValidatorAdditionTs
    
    * feat(BLK-3664): Merge multiple init functions into one per contract, deployment script for sherlock high risk batch 2 (#1)
    
    * Added scripts for Sherlock high risk 2nd batch
    
    * Fixed typo
    
    * Unified init functions in ChainConfig & Staking. Added tests
    
    * Updated scripts for Sherlock high risk batch 2 to use new init functions
    
    * WIP
    
    * Override ChainConfig & Staking contracts to bypass local simulation
    
    * Added missing commits from private gh, fixed tests, added deployment scripts.
    
    * Added missing comment

--- contracts/ChainConfig.sol
+++ contracts/ChainConfig.sol
@@ -5,9 +5,9 @@ import "./Injector.sol";
 
 contract ChainConfig is InjectorContextHolder, IChainConfig {
 
-    event ActiveValidatorsLengthChanged(uint32 prevValue, uint32 newValue);
+    event ActiveValidatorsLengthChanged(uint32 prevValue, uint32 newValue, uint64 epoch);
     event EpochBlockIntervalChanged(uint32 prevValue, uint32 newValue);
-    event MisdemeanorThresholdChanged(uint32 prevValue, uint32 newValue);
+    event MisdemeanorThresholdChanged(uint32 prevValue, uint32 newValue, uint64 epoch);
     event FelonyThresholdChanged(uint32 prevValue, uint32 newValue);
     event ValidatorJailEpochLengthChanged(uint32 prevValue, uint32 newValue);
     event UndelegatePeriodChanged(uint32 prevValue, uint32 newValue);
@@ -15,9 +15,9 @@ contract ChainConfig is InjectorContextHolder, IChainConfig {
     event MinStakingAmountChanged(uint256 prevValue, uint256 newValue);
 
     struct ConsensusParams {
-        uint32 activeValidatorsLength;
+        uint32 activeValidatorsLength; // depricated. use epochConsensusParams.activeValidatorLength
         uint32 epochBlockInterval;
-        uint32 misdemeanorThreshold;
+        uint32 misdemeanorThreshold; // depricated. use epochConsensusParams.misdemeanorThreshold
         uint32 felonyThreshold;
         uint32 validatorJailEpochLength;
         uint32 undelegatePeriod;
@@ -25,7 +25,20 @@ contract ChainConfig is InjectorContextHolder, IChainConfig {
         uint256 minStakingAmount;
     }
 
+    struct EpochToValue {
+        // (epoch => value)
+        mapping(uint64 => uint32) value;
+        // list of available epochs, sorted in asc order.
+        uint64[] epochs;
+    }
+
+    struct EpochConsensusParams {
+        EpochToValue activeValidatorLength;
+        EpochToValue misdemeanorThreshold;
+    }
+
     ConsensusParams private _consensusParams;
+    EpochConsensusParams private _epochConsensusParams;
 
     constructor(bytes memory constructorParams) InjectorContextHolder(constructorParams) {
     }
@@ -40,12 +53,14 @@ contract ChainConfig is InjectorContextHolder, IChainConfig {
         uint256 minValidatorStakeAmount,
         uint256 minStakingAmount
     ) external onlyInitializing {
-        _consensusParams.activeValidatorsLength = activeValidatorsLength;
-        emit ActiveValidatorsLengthChanged(0, activeValidatorsLength);
+        _epochConsensusParams.activeValidatorLength.value[0] = activeValidatorsLength;
+        _epochConsensusParams.activeValidatorLength.epochs.push(0);
+        emit ActiveValidatorsLengthChanged(0, activeValidatorsLength, 0);
         _consensusParams.epochBlockInterval = epochBlockInterval;
         emit EpochBlockIntervalChanged(0, epochBlockInterval);
-        _consensusParams.misdemeanorThreshold = misdemeanorThreshold;
-        emit MisdemeanorThresholdChanged(0, misdemeanorThreshold);
+        _epochConsensusParams.misdemeanorThreshold.value[0] = misdemeanorThreshold;
+        _epochConsensusParams.misdemeanorThreshold.epochs.push(0);
+        emit MisdemeanorThresholdChanged(0, misdemeanorThreshold, 0);
         _consensusParams.felonyThreshold = felonyThreshold;
         emit FelonyThresholdChanged(0, felonyThreshold);
         _consensusParams.validatorJailEpochLength = validatorJailEpochLength;
@@ -62,14 +77,46 @@ contract ChainConfig is InjectorContextHolder, IChainConfig {
         return _consensusParams;
     }
 
+    /// @notice depricated. use `getActiveValidatorsLength(uint64 epoch)` instead.
     function getActiveValidatorsLength() external view override returns (uint32) {
-        return _consensusParams.activeValidatorsLength;
+        return getActiveValidatorsLength(_stakingContract.currentEpoch());
+    }
+
+    function getActiveValidatorsLength(uint64 epoch) public view returns (uint32) {
+        EpochToValue storage avl = _epochConsensusParams.activeValidatorLength;
+        return _getValue(avl, epoch);
+    }
+
+    /// @notice Initialize active validators length and misdemeanor threshold per epoch variables.
+    function initEpochParams() public onlyFromRuntimeUpgrade {
+        EpochToValue storage avl = _epochConsensusParams.activeValidatorLength;
+        require(avl.epochs.length == 0, "AI"); // already initialized
+
+        EpochToValue storage mt = _epochConsensusParams.misdemeanorThreshold;
+        require(mt.epochs.length == 0, "AI"); // already initialized
+
+        // copy activeValidatorLength & misdemeanorThreshold to epoch 0 and to current epoch
+        avl.value[0] = _consensusParams.activeValidatorsLength;
+        avl.epochs.push(0);
+        mt.value[0] = _consensusParams.misdemeanorThreshold;
+        mt.epochs.push(0);
+        uint64 e = _stakingContract.currentEpoch();
+        if (e > 0) {
+            avl.value[e] = _consensusParams.activeValidatorsLength;
+            avl.epochs.push(e);
+            mt.value[e] = _consensusParams.misdemeanorThreshold;
+            mt.epochs.push(e);
+        }
     }
 
     function setActiveValidatorsLength(uint32 newValue) external override onlyFromGovernance {
-        uint32 prevValue = _consensusParams.activeValidatorsLength;
-        _consensusParams.activeValidatorsLength = newValue;
-        emit ActiveValidatorsLengthChanged(prevValue, newValue);
+        EpochToValue storage avl = _epochConsensusParams.activeValidatorLength;
+
+        // set new value for next epoch
+        uint32 prevValue = _getValue(avl, _stakingContract.currentEpoch());
+        uint64 nextEpoch = _stakingContract.nextEpoch();
+        _setValue(avl, newValue);
+        emit ActiveValidatorsLengthChanged(prevValue, newValue, nextEpoch);
     }
 
     function getEpochBlockInterval() external view override returns (uint32) {
@@ -80,10 +127,19 @@ contract ChainConfig is InjectorContextHolder, IChainConfig {
         return _consensusParams.misdemeanorThreshold;
     }
 
+    function getMisdemeanorThreshold(uint64 epoch) external view returns (uint32) {
+        EpochToValue storage mt = _epochConsensusParams.misdemeanorThreshold;
+        return _getValue(mt, epoch);
+    }
+
     function setMisdemeanorThreshold(uint32 newValue) external override onlyFromGovernance {
-        uint32 prevValue = _consensusParams.misdemeanorThreshold;
-        _consensusParams.misdemeanorThreshold = newValue;
-        emit MisdemeanorThresholdChanged(prevValue, newValue);
+        EpochToValue storage mt = _epochConsensusParams.misdemeanorThreshold;
+
+        // set new value for next epoch
+        uint32 prevValue = _getValue(mt, _stakingContract.currentEpoch());
+        uint64 nextEpoch = _stakingContract.nextEpoch();
+        _setValue(mt, newValue);
+        emit MisdemeanorThresholdChanged(prevValue, newValue, nextEpoch);
     }
 
     function getFelonyThreshold() external view override returns (uint32) {
@@ -134,5 +190,39 @@ contract ChainConfig is InjectorContextHolder, IChainConfig {
         uint256 prevValue = _consensusParams.minStakingAmount;
         _consensusParams.minStakingAmount = newValue;
         emit MinStakingAmountChanged(prevValue, newValue);
+    }
+
+    function _getValue(EpochToValue storage param, uint64 epoch) internal view returns (uint32) {
+        if (param.value[epoch] > 0) {
+            return param.value[epoch];
+        } else {
+            uint256 epochsLen = param.epochs.length;
+            uint64 lastAvailableEpoch = param.epochs[epochsLen - 1];
+            // If we don't have the value for the epoch, return the lastAvailable epoch value if possible.
+            // (actually, epoch == lastAvailableEpoch case should be covered by the if statement above, but whatever..)
+            if (epoch >= lastAvailableEpoch) {
+                return param.value[lastAvailableEpoch];
+            } else {
+                // If we don't have the value for the epoch and epoch < lastAvailable
+                // binary search to find the closest epoch
+                uint256 left = 0;
+                uint256 right = epochsLen;
+                while (left < right) {
+                     uint256 mid = left + (right - left) / 2;
+                     if (param.epochs[mid] <= epoch) {
+                         left = mid + 1;
+                     } else {
+                         right = mid;
+                     }
+                }
+                return param.value[param.epochs[left-1]];
+            }
+        }
+    }
+
+    function _setValue(EpochToValue storage param, uint32 value) internal {
+        uint64 nextEpoch = _stakingContract.nextEpoch();
+        param.value[nextEpoch] = value;
+        param.epochs.push(nextEpoch);
     }
 }
